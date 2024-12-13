71c5f5228fb0d8e5f8b9496033379aa6503c8564
Eyu-Gen <dreamersthebig@gmail.com>
2024-12-13 22:24:30 +0545

    Incompleted

--- app/page.js
+++ app/page.js
@@ -1,51 +1,65 @@
 "use client"
-import Image from "next/image";
-import { useState } from "react";
 
-export default function Home() {
-  //Enter garako task array ma rakhna...
-  const [ taskArr, setTask ] = useState([]);
-  function addToList() {
-    const enteredTask = document.getElementById("taskEnter").value;
-    
-      setTask(function(temp) {
-        const newArr = [...temp, enteredTask];
-        return newArr;
-      });
-  };
+import Image from 'next/image'
+import React, { useState } from 'react'
 
-  //Clear btn garda sabai task delete garna...
-  function clearList() {
-    document.getElementById("taskEnter").value = "";
-    setTask([]);
-  };
+const page = () => {
+  const [t, setT] = useState("");
+  const [taskArr, setTaskArr] = useState([]);
 
-  //Delete btn click garda task delete garna...
-  function deleteTaskFun(task) {
-    const newArr = taskArr.filter(t => t !== task);
-    setTask(newArr);
-  };
+  function addToDo(e) {
+    e.preventDefault();
 
-  //Task Dislpay garako...
+    setTaskArr(function(temp){
+      const newArr = [...temp, t];
+      return newArr;
+    })
+
+    setT("");
+  }
+
   function render() {
-    return(
-      taskArr.map((task, index) => {
-        return ( <li key={ task }> { task } <button id="deleteTask" onClick={ () => deleteTaskFun(task) }>Delete</button> </li>)
-      })
-    )
-  };
+    const elements = taskArr.map((task, index) => { 
+      return (
+        <>
+          <li key={task} className='flex items-center'>
+            <div className="w-5 h-5 rounded-full border-2 border-veryDarkGrayishBlue1 mr-3 cursor-pointer"></div>
+            <p> { task } </p>
+          </li>
+          {
+            index+1 == taskArr.length ? "" : <hr className='mb-3 mt-3 border-none outline-none h-[1px] bg-veryDarkGrayishBlue1'/>
+          } 
+          
+        </>
+      )
+     });
+
+     return elements;
+  }
 
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <form className="mb-20">
-        <input type="text" placeholder="Enter task here..." id="taskEnter" className="w-96 mb-2  border-black border-2 rounded p-5" /> <br />
-        <button type="button" onClick={ addToList } id="submitBtn" className="mr-10">ADD</button>
-        <button type="button" onClick={ clearList } id="submitBtn">CLEAR</button>
-      </form>
+    <main className='w-screen h-screen'>
+      <div className='w-screen h-60 bg-desktopBgDark bg-cover'></div>
+      <div className='mx-auto max-w-xl -mt-44'>
+        <div id="header" className='flex items-center justify-between w-full mb-7'>
+          <p id="heading" className='text-[2rem] font-semibold text-white -tracking-wide'>T O D O</p>
+          <Image src="icon-sun.svg" width={30} height={30} alt=""/>
+        </div>
+        <div id="inputContainer" className='w-full h-12 bg-veryDarkBlue rounded'>
+          <form className='w-full h-full' onSubmit={e=>addToDo(e)}>
+            <input onChange={ (e) => setT(e.target.value) } value={t} autoComplete='off' id="enteredTask" type="text" placeholder='Create a new todo...' className='text-white bg-transparent w-full h-full pl-5 focus:outline-none caret-white' />
+          </form>
+        </div>
+        {
+          taskArr.length > 0 && <div id="taskListContainer" className='w-full p-5 bg-veryDarkBlue mt-5 rounded shadow-[rgba(0, 0, 0,0.3)] shadow-lg text-white'>
+          <ol>
+            { render() }
+          </ol>
+        </div> 
+        }
+      </div>
+    </main>
+  )
+}
 
-      <ol>
-        { render() }
-      </ol>
-    </div>
-  );
-};
+export default page
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,9 +8,29 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "brightBlue": "var(--brightBlue)",
+        "gradientBg1": "var(--gradientBg1)",
+        "gradientBg2": "var(--gradientBg2)",
+        "darkBlue": "var(--darkBlue)",
+        "veryDarkBlue": "var(--veryDarkBlue)",
+        "darkGrayishBlue1": "var(--darkGrayishBlue1)",
+        "veryDarkGrayishBlue1": "var(--veryDarkGrayishBlue1)",
+        "veryDarkGrayishBlue2": "var(--veryDarkGrayishBlue2)",
+        "lightBlue": "var(--lightBlue)",
+        "lightBlueHover": "var(--lightBlueHover)",
+        "veryLightGray": "var(--veryLightGray)",
+        "veryLightGrayishBliue": "var(--veryLightGrayishBliue)",
+        "lightGrayishBlue": "var(--lightGrayishBlue)",
+        "darkGrayishBlue2": "var(--darkGrayishBlue2)",
+        "veryDarkGrayishBlue3": "var(--veryDarkGrayishBlue3)",
       },
+
+      backgroundImage: {
+        "desktopBgDark": "url('/bg-desktop-dark.jpg')",
+        "desktopBgLight": "url('/bg-desktop-light.jpg')",
+        "mobileBgDark": "url('/bg-mobile-dark.jpg')",
+        "mobileBgLight": "url('/bg-mobile-light.jpg')",
+      }
     },
   },
   plugins: [],
